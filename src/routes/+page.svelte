4b43121a4ff6d4fe144bfc4c24a47a32ee4f0cbc
Gio Meijer <meij0155@hz.nl>
2024-11-26 11:21:10 +0100

    Spellingsfout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,5 +5,5 @@
 <h1 class="bg-lime-400">Welcome to SvelteKit</h1>
 <p>
     Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-    documentations
+    documentation
 </p>
