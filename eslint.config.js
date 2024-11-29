aefcd9bf3f0f71a20240d656bffc878286dc2c49
renzo <renzo.debeer06@gmail.com>
2024-11-29 11:41:45 +0100

    test

--- eslint.config.js
+++ eslint.config.js
@@ -4,27 +4,29 @@ import pluginReact from "eslint-plugin-react";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  // Specificeer de bestanden die linten
   { files: ["**/*.{js,mjs,cjs,jsx}"] },
-  
-  // Definieer de globale variabelen voor de browseromgeving
-  { languageOptions: { globals: globals.browser } },
-
-  // Voeg de aanbevolen configuraties toe voor JavaScript en React
+  { languageOptions: { globals: { ...globals.browser, Buffer: "readonly" } } },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
-
-  // Voeg aanpassingen toe aan de regels van de linter
+  {
+    settings: {
+      react: {
+        version: "detect", // Automatically detect React version
+      },
+    },
+  },
   {
     rules: {
-      // Voeg team-specifieke regels toe of pas ze aan
-      "no-unused-vars": ["warn"], // Zet 'no-unused-vars' op 'warn' in plaats van 'error'
-      "no-self-assign": "off", // Schakel 'no-self-assign' uit
-      "no-empty": "off", // Schakel 'no-empty' uit
-      "no-useless-escape": "off", // Schakel 'no-useless-escape' uit
-      "@typescript-eslint/require-await": "off", // Schakel '@typescript-eslint/require-await' uit
-      "no-constant-condition": "off", // Schakel 'no-constant-condition' uit voor de gegenereerde code
-      "no-control-regex": "off", // Schakel 'no-control-regex' uit
-    }
-  }
+      "no-unused-vars": ["warn"],
+      "no-self-assign": "off",
+      "no-empty": "off",
+      "no-useless-escape": "off",
+      "@typescript-eslint/require-await": "off",
+      "no-constant-condition": "off",
+      "no-control-regex": "off",
+      "no-async-promise-executor": "off", // Allow async promise executor
+      "no-case-declarations": "off", // Allow case block lexical declarations
+      "no-extra-boolean-cast": "off", // Allow redundant double negation
+    },
+  },
 ];
