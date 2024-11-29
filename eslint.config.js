ec0e2f0e4286c0b3b1d793c82bce6d1538c9374f
renzo <renzo.debeer06@gmail.com>
2024-11-29 11:30:12 +0100

    test

--- eslint.config.js
+++ eslint.config.js
@@ -14,9 +14,9 @@ export default [
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
 
-  // Sluit de gegenereerde Svelte-bestanden uit
-  { 
-    ignorePatterns: [
+  // Gebruik 'ignores' om de gegenereerde Svelte-bestanden uit te sluiten
+  {
+    ignores: [
       ".svelte-kit/*",
       "node_modules/*"
     ]
@@ -29,7 +29,7 @@ export default [
       "no-self-assign": "off", // Schakel 'no-self-assign' uit
       "no-empty": "off", // Schakel 'no-empty' uit
       "no-useless-escape": "off", // Schakel 'no-useless-escape' uit
-      "@typescript-eslint/require-await": "off", // Schakel '@typescript-eslint/require-await' uit (als je deze rule hebt geconfigureerd)
+      "@typescript-eslint/require-await": "off", // Schakel '@typescript-eslint/require-await' uit
       "no-constant-condition": "off", // Schakel 'no-constant-condition' uit voor de gegenereerde code
       "no-control-regex": "off" // Schakel 'no-control-regex' uit
     }
