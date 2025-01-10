edf29d7ed4cf04a6c673420d3548f2742fb249d5
Gio Meijer <meij0155@hz.nl>
2025-01-10 10:23:05 +0100

    Shoppingcart in home pagina verwijdert

--- src/lib/components/co2/CO2Output.svelte
+++ src/lib/components/co2/CO2Output.svelte
@@ -12,19 +12,6 @@
                 <span class="font-semibold text-gray-900">CO₂ Contributie:</span> {$user.co2Contribution} kg
             </p>
         </div>
-
-        {#if $user.shoppingCart.length > 0}
-            <div class="mt-6">
-                <h2 class="text-xl font-semibold text-gray-700 mb-2">Shopping Cart:</h2>
-                <ul>
-                    {#each $user.shoppingCart as item}
-                        <li class="text-gray-700 text-lg">
-                            {item.name} - {item.amount} {item.unit}
-                        </li>
-                    {/each}
-                </ul>
-            </div>
-        {/if}
     {:else}
         <!-- Als de gebruiker niet is ingelogd -->
         <p class="text-gray-700 text-lg text-center">
